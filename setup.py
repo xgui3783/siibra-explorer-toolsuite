3e049a0068022e5345b6fec5a354d1a8b8b7ee86
Xiao Gui <xgui3783@gmail.com>
2022-01-21 10:13:30 +0100

    added setup details

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="siibra-explorer-toolsuite",
+    version="0.0.1a",
+    author="Xiao Gui",
+    author_email="xgui3783@gmail.com",
+    packages=find_packages(include=["siibra_explorer_toolsuite"]),
+    python_requires=">=3.6",
+    install_requires=[
+        "siibra==0.3a6"
+    ]
+)
